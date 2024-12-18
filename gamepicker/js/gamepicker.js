90ec1a3019df98c7b836d43111941c285561a647
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2024-12-17 18:44:04 -0600

    Game Picker - Update games

---
+++ gamepicker/js/gamepicker.js
@@ -0,0 +1,36 @@
+const movies = [
+  "Gears of War 1",
+  "Fifa 25",
+  "Fable 1",
+  "Fallout 3",
+  "Shadow of Mordor",
+  "Street Fighters",
+  "Army of Two",
+  "Gears of War Judgment",
+  "GTA Vice City",
+  "GTA V",
+  "Crash Team Racing",
+  "Skyrim",
+  "The Witcher 3",
+  "Red Dead Redemption",
+  "Palworld",
+  "Resident Evil 5",
+  "Subnautica",
+  "State of Decay",
+  "Metro Redux",
+  "Just Cause 2",
+  "Farcry 2",
+  "Resident Evil",
+  "Dead Rising",
+  "Terraria",
+  "GTA San Andreas",
+  "Dead Space",
+  "Stardew Valley",
+  "Yu-Gi-Oh! Legacy of the Duelist : Link Evolution",
+];
+
+const random = Math.floor(Math.random() * movies.length);
+
+var moviePicked = document.getElementById("moviePicked");
+
+moviePicked.textContent += movies[random];
